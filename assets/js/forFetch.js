e28d8e13b28b89ed9db2e0f64caa6c42abf9b446
Lovisgod <olifedayo94@gmail.com>
2020-02-20 07:17:00 +0100

    FT/login fixed

--- assets/js/forFetch.js
+++ assets/js/forFetch.js
@@ -1,40 +1,43 @@
-/*const loginUsername = document.getElementById('#login-user').value;
+// const loginUsername = document.getElementById('#login-user').value;
 
-const loginPassword = document.getElementById('#login-pass').value;
+// const loginPassword = document.getElementById('#login-pass').value;
 
-const submitLogin = document.getElementById('#login-button').value;
+// const submitLogin = document.getElementById('#login-button').value;
 
-submitLogin.addEventListener('click', function() {
-    console.log(loginUsername);
-    console.log(loginPassword);
-    console.log(submitLogin);
-});
+// submitLogin.addEventListener('click', function() {
+//     console.log(loginUsername);
+//     console.log(loginPassword);
+//     console.log(submitLogin);
+// });
 
-*/
-
-/*const submitLogin = document.getElementById('#login-button');
+const url = "https://corporate-setup.herokuapp.com/api/v1/user/login"
+const submitLogin = document.getElementById('login-button');
 
 submitLogin.addEventListener('click', function(e){
     e.preventDefault();
     
-    const loginUsername = document.getElementById('#login-user').value;
+    // const loginUsername = document.getElementById('login-user').value;
     
-    const loginEmail = document.getElementById('#login-email').value;
+    const loginEmail = document.getElementById('login-email').value;
 
-    const loginPassword = document.getElementById('#login-pass').value;
-    */
+    const loginPassword = document.getElementById('login-pass').value;
 
   const params = {
-    email: "olifedayo94@gmail.com",
-    password: "Lovisgod94"
+    email: loginEmail,
+    password: loginPassword
   };
-  fetch("https://corporate-setup.herokuapp.com/api/v1/user/login", {method:"POST", body:params})
-  .then(function(response){
-  let res = response.
-    console.log(response);
+  let formData = new FormData();
+  formData.append('email', loginEmail);
+  formData.append('password', loginPassword);
+  fetch(url, {method:"POST", body:formData})
+  .then(res => res.json())
+  .then(x => {
+    console.log(x.data);
+    if (x.status != 'error') {
+      // similar behavior as an HTTP redirect
+     localStorage.setItem('token', x.data); 
+     window.location.replace("index.html");
+    }
   })
   
-
-
-  
-
+})
