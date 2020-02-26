26a5867c9f9feb50131f6f8d00b0c8435b9a3669
victuk <ukokjnr@gmail.com>
2020-02-26 21:30:23 +0100

    There is now only one index page, ive added the javascript dom to get the login and register buttons in the condition.js file (so that they can be hidden when logged in).

--- assets/js/condition.js
+++ assets/js/condition.js
@@ -3,3 +3,10 @@ const token = localStorage.getItem('token');
 // hide them when there is token
 // unhide them when there is no token
 // unhide them when token expire
+
+// for the login button
+const loginButton = getElementById("login-button");
+
+//for create account button
+
+const createAccountButton = getElementById("create-account-button");
