04cf8df714fa83e3194bda286f23ed1079629b59
Lovisgod <olifedayo94@gmail.com>
2020-02-28 22:21:06 +0100

    FT/apply_service

--- assets/js/appliedServiceForm.js
+++ assets/js/appliedServiceForm.js
@@ -1,5 +1,7 @@
 // var e = document.getElementById("country");
 // var result = e.options[e.selectedIndex].value;
+const url = "https://corporate-setup.herokuapp.com/api/v1/service/create-service-request";
+const requests = [];
 
 function getOption() {
     var checkboxes = document.getElementsByName('service[]');
@@ -8,10 +10,65 @@ function getOption() {
     checkboxes[i].addEventListener('click', getParent);
   }
 
-  function getParent(){
-  console.log(this.value);
+  function getParent(){    
+  console.log(this.parentNode.innerText);
+  requests.push(this.parentNode.innerText);
  }
 }
+
+// const sector = document.getElementById("sector");
+// const company_name = document.getElementById("request_company");
+// const full_name = document.getElementById('full_name');
+// const request_email = document.getElementById('request_email');
+// const request_phone = document.getElementById("request_phone");
+// const request_country = document.getElementById("request_country");
+// const request_message = document.getElementById('request_message');
+const sub_button = document.getElementById('submit_request')
+
+sub_button.addEventListener('click', function (e){
+  e.preventDefault();
+  const theToken = localStorage.getItem('token');
+  const option_sel = document.getElementById("sector")
+  const country_sel = document.getElementById('request_country');
+  const sector = option_sel.options[option_sel.selectedIndex].innerText;
+  const company_name = document.getElementById("request_company").value;
+  const full_name = document.getElementById('full_name').value;
+  const request_email = document.getElementById('request_email').value;
+  const request_phone = document.getElementById("request_phone").value;
+  const request_country = country_sel.options[country_sel.selectedIndex].innerText;;
+  const request_message = document.getElementById('request_message').value;
+
+  console.log(sector, company_name, full_name, request_message, request_phone, request_email, request_country, requests);
+
+  let formData = new FormData();
+  formData.append('full_name', full_name);
+  formData.append('email', request_email);
+  formData.append('location', request_country);
+  formData.append('phone', request_phone);
+  formData.append('message', request_message);
+  formData.append('company_name', company_name);
+  formData.append('sector', sector);
+  formData.append('request', requests);
+  fetch(url, {
+      method:"POST", 
+      body:formData, 
+      headers: new Headers({
+        'Authorization': `Bearer ${theToken}`
+  }), })
+  .then(res => res.json())
+  .then(x => {
+    console.log(x.data);
+    if (x.status != 'error') {
+      // similar behavior as an HTTP redirect
+    //  localStorage.setItem('token', x.data); 
+     alert(x.data);
+     window.location.replace("index.html");
+    } else {
+      alert(JSON.stringify(x.error));
+    }
+  }) 
+})
+
 
 function getIndex(){ 
     const selectOption = document.getElementById("sector");
