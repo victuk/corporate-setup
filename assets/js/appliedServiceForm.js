350e4fe7a0c38a9de7d86a77950d253ea3982f7d
Lovisgod <olifedayo94@gmail.com>
2020-02-28 07:28:52 +0100

    FT/select options

--- assets/js/appliedServiceForm.js
+++ assets/js/appliedServiceForm.js
@@ -1,7 +1,17 @@
-var e = document.getElementById("country");
-var result = e.options[e.selectedIndex].value;
+// var e = document.getElementById("country");
+// var result = e.options[e.selectedIndex].value;
 
+function getOption() {
+    var checkboxes = document.getElementsByName('service[]');
+    console.log(checkboxes)
+    for(var i=0;i<checkboxes.length;i++){
+    checkboxes[i].addEventListener('click', getParent);
+  }
 
+  function getParent(){
+  console.log(this.value);
+ }
+}
 
 function getIndex(){ 
     const selectOption = document.getElementById("sector");
@@ -30,22 +40,23 @@ function getIndex(){
 </div>`;
     }
     if(selected == 4){
-        showProduct.innerHTML = `<div class="row" id="show_product"><div class="col-md-12"> <input type="checkbox" name="service[]" id="3" value="3"> INCORPORATIONS ( CAC,CAC 7,CAC2)</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="15" value="15"> TIN NUMBER</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="8" value="8"> OPEN A COMPANY BANK ACCOUNT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="6" value="6"> OFFICE ACQUISITION</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="16" value="16"> WEB DEVELOPMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="10" value="10"> PRE/POST ADVERTISEMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="14" value="14"> REGISTER WITH NAFDAC</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="13" value="13"> REGISTER COMPANY WITH STATE PAYEE</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="12" value="12"> REGISTER COMPANY WITH A PENSION ADMINISTRATOR</div> 
-
-</div>`;
-    }
-    if(selected == 5){
     
-    showProduct.innerHTML = `<div class="row" id="show_product"><div class="col-md-12"> <input type="checkbox" name="service[]" id="3" value="3"> INCORPORATIONS ( CAC,CAC 7,CAC2)</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="2" value="2"> DEPARTMENT OF PETROLEUM REGISTRATION/LICENSE</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="4" value="4"> NIMASA REGISTRATION</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="5" value="5"> NIPEX REGISTRATION</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="9" value="9"> OPEN A CURRENT ACCOUNT WITH A NIGERIAN BANK</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="8" value="8"> OPEN A COMPANY BANK ACCOUNT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="16" value="16"> WEB DEVELOPMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="6" value="6"> OFFICE ACQUISITION</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="10" value="10"> PRE/POST ADVERTISEMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="13" value="13"> REGISTER COMPANY WITH STATE PAYEE</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="12" value="12"> REGISTER COMPANY WITH A PENSION ADMINISTRATOR</div> 
-
-</div>`;
-    }
-    if(selected == 6){
+        showProduct.innerHTML = `<div class="row" id="show_product"><div class="col-md-12"> <input type="checkbox" name="service[]" id="3" value="3"> INCORPORATIONS ( CAC,CAC 7,CAC2)</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="15" value="15"> TIN NUMBER</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="6" value="6"> OFFICE ACQUISITION</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="8" value="8"> OPEN A COMPANY BANK ACCOUNT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="16" value="16"> WEB DEVELOPMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="10" value="10"> PRE/POST ADVERTISEMENT</div> 
     
-    showProduct.innerHTML = `<div class="row" id="show_product"><div class="col-md-12"> <input type="checkbox" name="service[]" id="3" value="3"> INCORPORATIONS ( CAC,CAC 7,CAC2)</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="15" value="15"> TIN NUMBER</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="2" value="2"> DEPARTMENT OF PETROLEUM REGISTRATION/LICENSE</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="9" value="9"> OPEN A CURRENT ACCOUNT WITH A NIGERIAN BANK</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="8" value="8"> OPEN A COMPANY BANK ACCOUNT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="7" value="7"> OFFICE ACQUISITION/OFFICE EQUIPMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="16" value="16"> WEB DEVELOPMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="10" value="10"> PRE/POST ADVERTISEMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="13" value="13"> REGISTER COMPANY WITH STATE PAYEE</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="12" value="12"> REGISTER COMPANY WITH A PENSION ADMINISTRATOR</div> 
-
-</div>`;
-    }
+    </div>`;
+        }
+        if(selected == 5){
     
+            showProduct.innerHTML = `<div class="row" id="show_product"><div class="col-md-12"> <input type="checkbox" name="service[]" id="3" value="3"> INCORPORATIONS ( CAC,CAC 7,CAC2)</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="15" value="15"> TIN NUMBER</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="6" value="6"> OFFICE ACQUISITION</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="8" value="8"> OPEN A COMPANY BANK ACCOUNT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="16" value="16"> WEB DEVELOPMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="10" value="10"> PRE/POST ADVERTISEMENT</div> 
+        
+        </div>`;
+            }
+            if(selected == 6){
+    
+                showProduct.innerHTML = `<div class="row" id="show_product"><div class="col-md-12"> <input type="checkbox" name="service[]" id="3" value="3"> INCORPORATIONS ( CAC,CAC 7,CAC2)</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="15" value="15"> TIN NUMBER</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="6" value="6"> OFFICE ACQUISITION</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="8" value="8"> OPEN A COMPANY BANK ACCOUNT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="16" value="16"> WEB DEVELOPMENT</div><div class="col-md-12"> <input type="checkbox" name="service[]" id="10" value="10"> PRE/POST ADVERTISEMENT</div> 
+            
+            </div>`;
+                }
 
+                getOption();
 }
