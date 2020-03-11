34ee32b52e65d02d13e4dcb57e95511a8322b5d9
victuk <ukokjnr@gmail.com>
2020-03-11 21:46:03 +0100

    made necessary corrections to the page

---
+++ assets/js/ourServices.js
@@ -0,0 +1,25 @@
+const incorporationEmail = document.getElementById('incorporation-email');
+
+const incorporationApplyButton = document.getElementById('modal-apply-button-incorporation');
+
+const wDesignEmail = document.getElementById('web-design-email');
+
+const wDesignButton = document.getElementById('modal-apply-button-website');
+
+const rBodiesEmail = document.getElementById('regulatory-bodies-email');
+
+const rBodiesButton = document.getElementById('modal-apply-button-regulatory-bodies');
+
+const newComIntroEmail = document.getElementById('new-company-introduction-email');
+
+const newComIntroButton = document.getElementById('modal-apply-button-for-new-company-introduction');
+
+const staffTrainEmail = document.getElementById('staff-training-email');
+
+const staffTrainButton = document.getElementById('modal-apply-button-for-staff-training');
+
+const profBidderEmail = document.getElementById('prof-bidder-email');
+
+const profBidderButton = document.getElementById('modal-apply-button-prof-bidder');
+
+
