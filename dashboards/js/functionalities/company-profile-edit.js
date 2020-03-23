a547720e2647544146498e5fd0b691fdb5f7c71a
victuk <ukokjnr@gmail.com>
2020-03-23 14:39:21 +0100

    Worked on company profile edit

--- dashboards/js/functionalities/company-profile-edit.js
+++ dashboards/js/functionalities/company-profile-edit.js
@@ -10,5 +10,69 @@ const uploadPicture = document.getElementById('upload-profile-picture-button');
 
 const companyBriefSummary = document.getElementById('company-brief-summary');
 
+const getProfile = () => {
+    const theToken = localStorage.getItem('token');
+    if (!theToken) {
+      alert('Please Login')
+      window.location.replace("../../login.html");
+    }
+    const url = "https://corporate-setup.herokuapp.com/api/v1/user/profile"
+   fetch(url, {
+      method:"GET", 
+      headers: new Headers({
+    'Authorization': `Bearer ${theToken}`
+    }),
+   })
+  .then(res => res.json())
+  .then(x => {
+    if (x.status != 'error') {
+      companyName.value = `${x.data.profile.name}`;
+      companyPhoneNumber.value = `${x.data.profile.phone}`;
+    //   email.value = `Email: <span style = "color: green;">${x.data.profile.email}</span>`;
+    companyBriefSummary.value = `${x.data.profile.companyBriefDescription}`;
+    } else if (x.status == 'error') {
+      const message = x.error.message == "jwt expired" ? "Please Login to perform this operation" : x.error
+      if (authErrors.includes(message)) { window.location.replace("../login.html") }
+      alert(message);
+    }
+  })
+}
 
+getProfile();
 
+updateButton.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log(companyBriefSummary.value);
+    const updateProfile = (companyPhoneNumber, companyName, companyBriefSummary) => {
+        const theToken = localStorage.getItem('token');
+        if (!theToken) {
+          alert('Please Login')
+          window.location.replace("login.html");
+        }
+        console.log(email, service);
+        const url = "https://corporate-setup.herokuapp.com/api/v1/user/update-user"
+      let formData = new FormData();
+      formData.append('company_phone_number', companyPhoneNumber);
+      formData.append('company_name', companyName);
+      formData.append('company_summary', companyBriefSummary);
+      fetch(url, {
+          method:"POST", 
+          body:formData, 
+          headers: new Headers({
+        'Authorization': `Bearer ${theToken}`
+        }),
+    })
+      .then(res => res.json())
+      .then(x => {
+        console.log(x.data);
+        if (x.status != 'error') {
+          alert(x.data)
+          window.location.replace("company-profile.html")
+        } else if (x.status == 'error') {
+          const message = x.error.message == "jwt expired" ? "Please Login to perform this operation" : x.error
+          if (authErrors.includes(message)) { window.location.replace("../login.html") }
+          alert(message);
+        }
+      })
+    }
+});
