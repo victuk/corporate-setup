c4e64eb4f07becec89c6e63b4a98fa8d949eca39
Lovisgod <olifedayo94@gmail.com>
2020-03-24 17:04:25 +0100

    FT/search for user

--- dashboards/js/functionalities/for-all-searches.js
+++ dashboards/js/functionalities/for-all-searches.js
@@ -4,15 +4,41 @@ const searchACompanyButton = document.getElementById('search-a-company-button');
 
 const companyList = document.getElementById('company-list');
 
+
+const inflate = (data) => {
+  return`<option value="${data.email}">${data.name}</option>`;
+}
+
 userDropDown = () =>{
-    companyList.innerHTML =`
-    
-    <option value="Becky@gmail.com">Becky</option>
-    <option value="Agatevureglory@gmail.com">Glory</option>
-    <option value="Justin@gmail.com">Justin</option>
-    <option value="Ikpejs@gmail.com">Mercyikpe</option>
-    <option value="lovisgod94@gmail.com">Loveisgod</option>`;
-    console.log(searchACompany.value);
+  const theToken = localStorage.getItem('token');
+  if (!theToken) {
+    alert('Please Login')
+    window.location.replace("admin.html");
+  }
+  const url = "https://corporate-setup.herokuapp.com/api/v1/user/search-user";
+ fetch(`${url}?input=${searchACompany.value}`, {
+    method:"GET", 
+    headers: new Headers({
+  'Authorization': `Bearer ${theToken}`
+  }),
+ })
+.then(res => res.json())
+.then(x => {  
+  if (x.status != 'error') {
+    console.log(x.data);
+   const y = [];
+   x.data.forEach(el => {
+     const element = inflate(el);
+     y.push(element);
+   });
+   companyList.innerHTML = y;
+  } else if (x.status == 'error') {
+    const message = x.error.message == "jwt expired" ? "Please Login to perform this operation" : x.error
+    if (authErrors.includes(message)) { window.location.replace("../login.html") }
+    alert(message);
+  }
+})
+   
   }
 
   searchACompanyButton.addEventListener('click', function(e){
